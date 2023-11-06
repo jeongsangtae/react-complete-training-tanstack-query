cc8433a63aa3ded0c540613846fb8610198d3306
jeongsangtae <wjdtkdxo55@naver.com>
2023-11-07 04:26:45 +0900

    React Complete Training Tanstack Query - Fetch and output data using useQuery (EventDetails.jsx, http.js edit)

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,8 +1,20 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
-import Header from '../Header.jsx';
+import Header from "../Header.jsx";
+import { fetchEvent } from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
+  const params = useParams();
+  const id = params.id;
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", { id: id }],
+    queryFn: ({ signal }) => fetchEvent({ id, signal }),
+  });
+
   return (
     <>
       <Outlet />
@@ -11,25 +23,34 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+      {isPending && <LoadingIndicator />}
+      {!isPending && data && (
+        <article id="event-details">
+          <header>
+            <h1>{data.title}</h1>
+            <nav>
+              <button onClick={deleteHandler}>Delete</button>
+              <Link to="edit">Edit</Link>
+            </nav>
+          </header>
+          <div id="event-details-content">
+            <img src="http://localhost:3000/" alt="" />
+            <div id="event-details-info">
+              <div>
+                <p id="event-details-location">{data.location}</p>
+                <time dateTime={`Todo-DateT$Todo-Time`}>{data.time}</time>
+              </div>
+              <p id="event-details-description">{data.description}</p>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
           </div>
-        </div>
-      </article>
+        </article>
+      )}
+      {isError && (
+        <ErrorBlock
+          title="An error occurred"
+          message={error.info?.message || "Failed to fetch events."}
+        />
+      )}
     </>
   );
 }
